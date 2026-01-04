147e5aa59f26e1f9f666778b7201270740c4bfd3
Evan Ellison <EvanMichaelEllison@gmail.com>
2026-01-04 16:15:00 -0500

    Theme creation tweaks

--- template/src/vite-env.d.ts
+++ template/src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+// Type declaration for vite-plugin-eslint (no @types package available)
+declare module 'vite-plugin-eslint' {
+  import { Plugin } from 'vite';
+  export default function eslint(options?: Record<string, unknown>): Plugin;
+}
